fe4c1b5ba2091ed2668f53c5efd8b6a6cbcca001
Surya Dhanush <46414110+suryaviyyapu@users.noreply.github.com>
2024-08-16 18:50:53 -0400

    Update page.tsx with S3 samples

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -6,15 +6,19 @@ import PageHeading from "@/app/components/shared/PageHeading";
 const images = [
   {
     id: "1",
-    src: "https://images.unsplash.com/photo-1585618256754-241cfe4e8113?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=100",
+    src: "https://sfolio.s3.amazonaws.com/GPTempDownloadNMcd.jpg",
   },
   {
     id: "2",
-    src: "https://images.unsplash.com/photo-1585619203238-70e7631cc672?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwcm9maWxlLXBhZ2V8OXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    src: "https://sfolio.s3.amazonaws.com/GPTempDownloadblur.jpg",
   },
   {
     id: "3",
-    src: "https://images.unsplash.com/photo-1585619443911-c2bb23fb2a49?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+    src: "https://sfolio.s3.amazonaws.com/GPTempDownloadNMcd.jpg",
+  },
+  {
+    id: "4",
+    src: "https://sfolio.s3.amazonaws.com/GPTempDownloadNMcd.jpg",
   },
 ];
 
