0a2fb14ef35d4ca23a4949cb96b506296b869d9e
Surya Dhanush <46414110+suryaviyyapu@users.noreply.github.com>
2024-08-19 01:33:08 -0400

    Updated to include not found ducky for photos.

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -2,6 +2,7 @@ import { Slide } from "../animation/Slide";
 import Image from "next/image";
 import { Metadata } from "next";
 import PageHeading from "@/app/components/shared/PageHeading";
+import EmptyState from "../components/shared/EmptyState";
 
 const images = [
   {
@@ -64,21 +65,25 @@ export default function Photos() {
     <main className="max-w-7xl mx-auto md:px-16 px-6 lg:mt-32 mt-20">
       <PageHeading
         title="Photos"
-        description="This page is still under construction..."
+        description="Dive into my personal gallery featuring landscapes, portraits, and candid moments. These photos represent my journey and passion for photography, capturing the world as I see it."
       />
       <figure className="my-6">
-        <Slide delay={0.12} className="flex flex-wrap gap-2">
-          {images.map((image) => (
-            <Image
-              key={image.id}
-              src={image.src}
-              alt="playing guitar"
-              width={350}
-              height={800}
-              className="dark:bg-primary-bg bg-secondary-bg"
-            />
-          ))}
-        </Slide>
+        {images.length > 0 ? (
+          <Slide delay={0.12} className="flex flex-wrap gap-2">
+            {images.map((image) => (
+              <Image
+                key={image.id}
+                src={image.src}
+                alt="Photo"
+                width={350}
+                height={800}
+                className="dark:bg-primary-bg bg-secondary-bg"
+              />
+            ))}
+          </Slide>
+        ) : (
+          <EmptyState value="Photos" />
+        )}
       </figure>
     </main>
   );
